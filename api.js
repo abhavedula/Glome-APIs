9672696e4e25a8da41f8bdce7778ca4d587e7580
Abha Vedula <abhavedula@abhas-mbp.lan>
2021-10-28 12:00:41 -0700

    Add twilio webhook

--- api.js
+++ api.js
@@ -1821,7 +1821,7 @@ const MessagingResponse = require('twilio').twiml.MessagingResponse;
 app.post('/sms', (req, res) => {
   const twiml = new MessagingResponse();
 
-  twiml.message('message response');
+  twiml.message();
 
   res.writeHead(200, {'Content-Type': 'text/xml'});
   res.end(twiml.toString());
